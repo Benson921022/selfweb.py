d2302494718b4b3e0ffc8cdc1a905c44839b0b84
Benson921022 <162430445+Benson921022@users.noreply.github.com>
2025-04-26 14:30:51 +0800

    Delete api directory

--- api/index.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-
-# 正確格式：定義 app，Vercel 會自動辨識這個變數
-app = Flask(__name__, template_folder="../templates")
-
-@app.route("/")
-def home():
-    return render_template("index.html")
